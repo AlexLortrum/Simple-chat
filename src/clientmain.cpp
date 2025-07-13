bacad7e3bd44149533686ad947aca37023962c89
AlexLortrum <alexanderilyushechky@yandex.ru>
2025-07-13 10:24:16 +0000

    Update clientmain.cpp

--- src/clientmain.cpp
+++ src/clientmain.cpp
@@ -1,44 +1,49 @@
 #include <iostream>
 #include <thread>
 #include <string>
+#include <cstring>
 #include "../include/client.h"
 #include "../include/other.h"
+#include "../include/socket.h"
+
 void recvMessage(Client sock)
 {
-	std::string buffer;
+	struct message buffer;
 	while (1)
 	{
 		if (sock.recv(&buffer)) break;
-		std::cout << buffer << std::endl;
+		std::cout << "[" << buffer.id << ":" << buffer.username << "] " << buffer.text << std::endl;
 	}
 }
 
-void sendMessage(Client sock)
+void sendMessage(Client sock, char username[NAME_SIZE])
 {
+	struct message message;
+	message.id = 0;
+	strcpy(message.username, username);
 	std::string buffer;
 	while (1)
 	{
 		getline(std::cin, buffer);
-		sock.send(buffer);
+		strcpy(message.text, buffer.c_str());
+		sock.send(message);
 		if (buffer.compare("exit") == 0) break;
 	}
 }
 
 int main(int argc, char* argv[])
 {
-	if (argc < 3)
+	if (argc < 4)
 	{
-		printf("usage: %s ip_address port\n", argv[0]);
+		printf("usage: %s ip_address port username\n", argv[0]);
 		return 1;
 	}
 	Client client(AF_INET, std::string(argv[1]), char_to_int(argv[2]));
-	if (client.connect(AF_INET, SOCK_STREAM, 0) == 1) return 1;
-	std::thread sendThread(sendMessage, client);
+	if (client.connect(AF_INET, SOCK_STREAM, 0, argv[3]) == 1) return 1;
+	std::thread sendThread(sendMessage, client, argv[3]);
 	std::thread recvThread(recvMessage, client);
 	recvThread.join();
 	sendThread.join();
 	client.close();
-	return 0;
-		
-	
+	return 0;	
 }
