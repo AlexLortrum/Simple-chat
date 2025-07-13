1a0cdf44d5e891f42ac3509e468f11b3e7a03154
AlexLortrum <alexanderilyushechky@yandex.ru>
2025-07-13 10:42:14 +0000

    Update client.h

--- include/client.h
+++ include/client.h
@@ -9,9 +9,9 @@ class Client
 {
 	public:
 		Client(int domain, std::string ip, uint16_t port);
-		int connect(int domain, int type, int protocol);
-		int recv(std::string *message);
-		int send(std::string message);
+		int connect(int domain, int type, int protocol, char username[NAME_SIZE]);
+		int recv(struct message *message);
+		int send(struct message message);
 		void close();
 	private:
 		struct sockaddr_in serv_addr;
