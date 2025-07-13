c7c015e091688716a1e14b770ee251723c558543
AlexLortrum <alexanderilyushechky@yandex.ru>
2025-07-13 10:44:36 +0000

    Update server.h
    Create struct user
    Remove getfd and getfds

--- include/server.h
+++ include/server.h
@@ -6,22 +6,28 @@
 #include <vector>
 #include "socket.h"
 
+struct user 
+{
+	int fd;
+	int id;
+	char username[NAME_SIZE];
+};
+
 class Server
 {
 	public:
 		Server(int domain, std::string ip, uint16_t port);
 		int init(int domain, int type, int protocol);
 		int accept();
-		int recv(int fd, std::string *message);
-		int send(int fd, std::string message);
-		void sendAll(std::string message);
-		std::vector<int> getfds();
-		int getfd(int index);
+		int recv(int fd, struct message *message);
+		int send(int fd, struct message message);
+		void sendAll(struct message message);
+		int getuserid(int fd);
 		int close(int index);
 		void closeAll();
 	private:
 		struct sockaddr_in serv_addr;
 		Socket serversock;
-		std::vector<int> fds;
+		std::vector<user> users;
 };
 #endif
