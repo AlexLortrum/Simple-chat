71288a1b0f123562fc4c71684a8b0897e49af259
AlexLortrum <alexanderilyushechky@yandex.ru>
2025-07-13 10:52:31 +0000

    Update client.cpp

--- src/client.cpp
+++ src/client.cpp
@@ -11,22 +11,29 @@ Client::Client(int domain, std::string ip, uint16_t port)
 	serv_addr.sin_addr.s_addr = inet_addr(ip.c_str());
 	serv_addr.sin_port = htons(port);
 }
-int Client::connect(int domain, int type, int protocol)
+
+int Client::connect(int domain, int type, int protocol, char username[NAME_SIZE])
 {
 	if (clientsock.init(domain, type, protocol) == 1) return 1;
 	if (clientsock.connect((struct sockaddr*) &serv_addr, sizeof(serv_addr)) == 1) return 1;
+	struct message message = {0};
+	strcpy(message.username, username);
+	if (clientsock.send(message) == -1) return 1;
 	return 0;
 }
-int Client::recv(std::string *message)
+
+int Client::recv(struct message *message)
 {
-	if (clientsock.recv(message)) return 1;
+	if (clientsock.recv(message) == 1) return 1;
 	return 0;
 }
-int Client::send(std::string message)
+
+int Client::send(struct message message)
 {
-	if (clientsock.send(message)) return 1;
+	if (clientsock.send(message) == 1) return 1;
 	return 0;
 }
+
 void Client::close()
 {
 	clientsock.close();
