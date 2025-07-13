d7257141bebe96296e38c8d6abb257f79c47c8b6
AlexLortrum <alexanderilyushechky@yandex.ru>
2025-07-13 10:38:17 +0000

    Update server.cpp

--- src/server.cpp
+++ src/server.cpp
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <vector>
 #include "../include/server.h"
+#include "../include/other.h"
 
 Server::Server(int domain, std::string ip, uint16_t port)
 {
@@ -15,26 +16,48 @@ Server::Server(int domain, std::string ip, uint16_t port)
 	serv_addr.sin_addr.s_addr = inet_addr(ip.c_str());
 	serv_addr.sin_port = htons(port);
 }
+
 int Server::init(int domain, int type, int protocol)
 {
 	if (serversock.init(domain, type, protocol) == 1) return 1;
-	if (serversock.bind((struct sockaddr*) &serv_addr, sizeof(serv_addr)) == 1) return 1; 
+	if (serversock.bind((struct sockaddr*) &serv_addr, sizeof(serv_addr)) == 1) return 1;
 	if (serversock.listen(5) == 1) return 1;
-	fds.push_back(serversock.getsockfd());
-	return 0;
+	struct user listener = {0};
+	listener.fd = serversock.getsockfd();
+	listener.id = 0;
+	users.push_back(listener);
+	return listener.fd;
 }
+
 int Server::accept()
 {
 	int clnt_sock = serversock.accept(NULL, NULL);
 	if (clnt_sock < 0) return 1;
-	fds.push_back(clnt_sock);
+	struct message userdata;
+	struct timeval tv;
+	tv.tv_sec = 1;
+	tv.tv_usec = 0;
+	setsockopt(clnt_sock, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+	if (recv(clnt_sock, &userdata) == 1) 
+	{
+		::close(clnt_sock);
+		return -1;
+	}
+	tv.tv_sec = 0;
+	setsockopt(clnt_sock, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+
+	struct user user;
+	user.fd = clnt_sock;
+	user.id = ::hash(userdata.username, clnt_sock);
+	strcpy(user.username, userdata.username);
+	users.push_back(user);
 	return clnt_sock;
 }
 
-int Server::recv(int fd, std::string *message)
+int Server::recv(int fd, struct message *message)
 {
-	char buffer[BUFFER_SIZE] = {0};
-	int res = ::recv(fd, buffer, BUFFER_SIZE, 0);
+	struct message buffer;
+	int res = ::recv(fd, &buffer, sizeof(struct message), 0);
 	*message = buffer;
 	lseek(fd, 0, SEEK_END);
 	if (res == -1)
@@ -50,9 +73,9 @@ int Server::recv(int fd, std::string *message)
 	return 0;
 }
 
-int Server::send(int fd, std::string message)
+int Server::send(int fd, struct message message)
 {
-	int res = ::send(fd, message.c_str(), message.size(), 0);
+	int res = ::send(fd, &message, sizeof(struct message), 0);
 	if (res == -1)
 	{
 		std::cout << "Message send failed\nCode:" << errno << std::endl;
@@ -61,37 +84,34 @@ int Server::send(int fd, std::string message)
 	return 0;
 }
 
-void Server::sendAll(std::string message)
+void Server::sendAll(struct message message)
 {
-	for (int i = 1; i < fds.size(); i++)
+	for (int i = 1; i < users.size(); i++)
 	{
-		send(fds[i], message);
+		send(users[i].fd, message);
 	}
 }
 
-std::vector<int> Server::getfds()
+int Server::getuserid(int fd)
 {
-	return fds;
-}
-
-int Server::getfd(int index)
-{
-	if (index > fds.size()) return -1;
-	return fds[index];
+	for (int i = 0; i < users.size(); i++)
+	{
+		if (users[i].fd == fd) return users[i].id;
+	}
+	return -1;
 }
 
 int Server::close(int index)
 {
-	if (index > fds.size()) return -1;
-	::close(fds[index]);
-	fds.erase(fds.begin() + index);
+	::close(users[index].fd);
+	users.erase(users.begin() + index);
 	return 0;
 }
 
 void Server::closeAll()
 {
-	for (int i = fds.size()-1; i >= 0; i--)
+	for (int i = users.size()-1; i >= 0; i--)
 	{
-		::close(fds[i]);
+		::close(users[i].fd);
 	}
 }
