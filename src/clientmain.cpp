238fb3d85f5cca3b6e3b331a2f7c13a6966eda92
AlexLortrum <alexanderilyushechky@yandex.ru>
2025-09-01 19:11:28 +0000

    feat(ui): Add user interface

--- src/clientmain.cpp
+++ src/clientmain.cpp
@@ -1,18 +1,36 @@
 #include <iostream>
 #include <thread>
+#include <mutex>
 #include <string>
 #include <cstring>
 #include "../include/client.h"
 #include "../include/other.h"
 #include "../include/socket.h"
+#include "../include/terminal.h"
+
+std::mutex mtx;
 
 void recvMessage(Client sock)
 {
 	struct message buffer;
+	int y = 1;
+	int maxy;
+	gety(&maxy);
 	while (1)
 	{
 		if (sock.recv(&buffer)) break;
+		mtx.lock();
+		if (y == maxy)
+		{
+			y--;
+			restore_cur();
+			printf("\n");
+		}
+		mvcurs(y, 0);
 		std::cout << "[" << buffer.id << ":" << buffer.username << "] " << buffer.text << std::endl;
+		y++;
+		if (y == maxy) save_cur();
+		mtx.unlock();
 	}
 }
 
@@ -20,14 +38,46 @@ void sendMessage(Client sock, char username[NAME_SIZE])
 {
 	struct message message;
 	message.id = 0;
+	memset(message.text, 0, BUFFER_SIZE);
 	strcpy(message.username, username);
-	std::string buffer;
+	std::string buffer = "";
+	char ch = 0;
+	int y;
+	int x = 1;
+	gety(&y);
 	while (1)
 	{
-		getline(std::cin, buffer);
+		while (ch != '\n')
+		{
+			ch = getchar();
+			if (ch == 127 && x > 1) // backspace
+			{
+				x--;
+				buffer.pop_back();
+				mtx.lock();
+				mvcurs(y, x);
+				printf(" ");
+				mvcurs(y, x);
+				mtx.unlock();
+			}
+			else if (x != BUFFER_SIZE-1 && ch != '\n')
+			{
+				buffer.push_back(ch);
+				mtx.lock();
+				mvcurs(y, x);
+				printf("%c", ch);
+				x++;
+				mtx.unlock();
+			}
+		}
 		strcpy(message.text, buffer.c_str());
 		sock.send(message);
 		if (buffer.compare("exit") == 0) break;
+		buffer.erase();
+		ch = 0;
+		x = 1;
+		mvcurs(y, 0);
+		clear_line();
 	}
 }
 
@@ -40,10 +90,12 @@ int main(int argc, char* argv[])
 	}
 	Client client(AF_INET, std::string(argv[1]), char_to_int(argv[2]));
 	if (client.connect(AF_INET, SOCK_STREAM, 0, argv[3]) == 1) return 1;
+	terminit();
 	std::thread sendThread(sendMessage, client, argv[3]);
 	std::thread recvThread(recvMessage, client);
 	recvThread.join();
 	sendThread.join();
 	client.close();
+	termexit();
 	return 0;	
 }
